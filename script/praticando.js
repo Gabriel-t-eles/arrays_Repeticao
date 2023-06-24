3549aa9d2cf9d7bcec7054c0c6f43c3527768b87
Gabriel-t-eles <131554267+Gabriel-t-eles@users.noreply.github.com>
2023-06-23 21:51:53 -0300

    Add files via upload

---
+++ script/praticando.js
@@ -0,0 +1,17 @@
+
+/*
+const numero = 8;
+for (let i = 1; i <= 10; i++) {
+    let tabuada = numero * i;
+    console.log(tabuada);  
+}
+*/
+
+const numeros = [0,1,2,3,4,5,6,7,8,9,10,11,50];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    if (numero % 2 === 0){
+        console.log(numero);
+    }
+}
